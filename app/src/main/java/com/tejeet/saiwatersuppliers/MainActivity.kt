033b4f3e4d5645c26f1d5eeb3f3e33c18d84d698
manishkumar <manishsahu354@gmail.com>
2021-06-25 19:26:57 +0530

    bottom navigation and navigation drawar added

--- app/src/main/java/com/tejeet/saiwatersuppliers/MainActivity.kt
+++ app/src/main/java/com/tejeet/saiwatersuppliers/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.tejeet.saiwatersuppliers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.tejeet.saiwatersuppliers.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sideNavigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawer,binding.myToolbar,R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        binding.drawer.addDrawerListener(toggle)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.orderFragment,
+                R.id.profileFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setSupportActionBar(binding.myToolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+
+            R.id.societyFragment -> {
+
+                binding.drawer.closeDrawer(GravityCompat.START)
+                startActivity(Intent(this@MainActivity, SocietiesActivity::class.java))
+                //overridePendingTransition(R.anim.enter_first, R.anim.enter_second)
+                true
+            }
+            R.id.driversFragment -> {
+                binding.drawer.closeDrawer(GravityCompat.START)
+                startActivity(Intent(this@MainActivity, DriversActivity::class.java))
+                //overridePendingTransition(R.anim.enter_first, R.anim.enter_second)
+                true
+            }
+            R.id.settingFragment -> {
+                binding.drawer.closeDrawer(GravityCompat.START)
+                startActivity(Intent(this@MainActivity, SettingActivity::class.java))
+               // overridePendingTransition(R.anim.enter_first, R.anim.enter_second)
+                true
+            }
+            R.id.logout -> {
+                binding.drawer.closeDrawer(GravityCompat.START)
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
---
+++ app/src/main/java/com/tejeet/saiwatersuppliers/SocietiesActivity.kt
@@ -0,0 +1,11 @@
+package com.tejeet.saiwatersuppliers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocietiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sovieties)
+    }
+}
